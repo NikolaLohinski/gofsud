1a8aec0d5675f0c2ff063c6ef23ba729ec37548e
Kevin LARQUEMIN <kevin.larquemin@stack-labs.com>
2020-11-13 17:04:37 +0100

    fix(mage): docker:package does not expand environment variables

--- spellbook/docker.go
+++ spellbook/docker.go
@@ -1,6 +1,8 @@
 package spellbook
 
 import (
+	"os"
+
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
@@ -9,15 +11,35 @@ type Docker mg.Namespace
 
 // Build docker image.
 func (Docker) Package() error {
+	goImageVersion := os.Getenv("GO_IMAGE_VERSION")
+	if goImageVersion == "" {
+		goImageVersion = "1.15.3-alpine"
+	}
+
+	distrolessImage := os.Getenv("DISTROLESS_IMAGE")
+	if distrolessImage == "" {
+		distrolessImage = "gcr.io/distroless/static"
+	}
+
+	distrolessVersion := os.Getenv("DISTROLESS_VERSION")
+	if distrolessVersion == "" {
+		distrolessVersion = "nonroot"
+	}
+
+	imageDestination := os.Getenv("IMAGE_DESTINATION")
+	if imageDestination == "" {
+		imageDestination = "theagentk/gofsud"
+	}
+
 	return sh.RunV(
 		"docker",
 		"build",
 		".",
-		"--build-arg", "GO_IMAGE_VERSION=${GO_IMAGE_VERSION:-1.15.3-alpine}",
-		"--build-arg", "DISTROLESS_IMAGE=${DISTROLESS_IMAGE:-gcr.io/distroless/base-debian10}",
-		"--build-arg", "DISTROLESS_VERSION=${DISTROLESS_VERSION:-nonroot}",
-		"--tag", "${IMAGE_DESTINATION:-theagentk/gofsud}:"+determineVersion(),
-		"--tag", "${IMAGE_DESTINATION:-theagentk/gofsud}:latest",
+		"--build-arg", "GO_IMAGE_VERSION="+goImageVersion,
+		"--build-arg", "DISTROLESS_IMAGE="+distrolessImage,
+		"--build-arg", "DISTROLESS_VERSION="+distrolessVersion,
+		"--tag", imageDestination+":"+determineVersion(),
+		"--tag", imageDestination+":latest",
 	)
 }
 
